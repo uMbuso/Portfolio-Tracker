92a2daf4ff7ed94caa8c4019fc2b17fee9dcb8ca
mbuso nkosi <mbusonkosi26@gmail.com>
2025-11-22 15:55:13 +0200

    Adds entities

---
+++ Portfolio Tracker API/Models/Investment.cs
@@ -0,0 +1,18 @@
+﻿namespace Portfolio_Tracker_API.Models
+{
+    public class Investment
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string Coin { get; set; }
+        public decimal Amount { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public string AssetType { get; set; }
+
+        public User User { get; set; }
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+
+}
---
+++ Portfolio Tracker API/Models/Transaction.cs
@@ -0,0 +1,14 @@
+﻿namespace Portfolio_Tracker_API.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int InvestmentId { get; set; }
+        public string Process { get; set; }
+        public decimal Amount { get; set; }
+        public decimal Price { get; set; }
+        public DateTime Date { get; set; }
+
+        public Investment Investment { get; set; }
+    }
+}
